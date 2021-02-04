55f894c27d84d0bb5ef828d6c0f783dba40bab41
senc1yo <senc1yo@users.noreply.github.com>
2021-02-04 21:58:06 +0100

    TuorialDialogue

---
+++ GameJam-2D/Assets/Scripts/Dialogue/UnityEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UnityEventHandler : MonoBehaviour
+{
+    [HideInInspector] public UnityEvent eventHandler;
+    [HideInInspector] public DialogueBase myDialogue;
+
+    public void InitiateAction()
+    {
+        StartCoroutine(InDialogueBuffer());
+    }
+
+    IEnumerator InDialogueBuffer()
+    {
+        yield return new WaitForSeconds(0.01f);
+
+        eventHandler.Invoke();
+
+        DialogueManager.instance.inDialogue = false;
+
+        if (myDialogue != null)
+        {
+            DialogueManager.instance.EnqueueDialogue(myDialogue);
+        }
+    }
+}
