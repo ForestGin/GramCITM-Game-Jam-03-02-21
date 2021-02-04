8441df729d0af3f410457ed350722f502a6eb73d
Pol Bosch <pol.hcsob@gmail.com>
2021-02-04 22:38:52 +0100

    Fixed shooting

--- GameJam-2D/Assets/Scripts/PlayerShooting.cs
+++ GameJam-2D/Assets/Scripts/PlayerShooting.cs
@@ -8,8 +8,12 @@ public class PlayerShooting : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletprefab;
 
-
+    public bool shoot = false;
     public float bulletForce = 100f;
+
+    public float time_btw_shots = 0.1f;
+    public float time_last_shot = 0;
+    public float timeinmilisec = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +24,20 @@ public class PlayerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
-        {
-            Shoot();
-        }
+        if (Input.GetMouseButton(0)) Shoot();
+
+        timeinmilisec += Time.deltaTime;
     }
+
     void Shoot()
     {
-        GameObject bullet = Instantiate(bulletprefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D bullet_rb = bullet.GetComponent<Rigidbody2D>();
-        bullet_rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        if (timeinmilisec > time_last_shot + time_btw_shots)
+        {
+            GameObject bullet = Instantiate(bulletprefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D bullet_rb = bullet.GetComponent<Rigidbody2D>();
+            bullet_rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+            time_last_shot = timeinmilisec + time_btw_shots;
+        }
     }
 }
