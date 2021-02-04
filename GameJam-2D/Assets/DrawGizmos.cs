fab1cd81807b453117fddc46d58c658c0e5b84ba
allenbateman <57528826+allenbateman@users.noreply.github.com>
2021-02-04 19:26:23 +0100

    Level1

---
+++ GameJam-2D/Assets/DrawGizmos.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGizmos : MonoBehaviour
+{
+    public float radius;
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+}
---
+++ GameJam-2D/Assets/Scripts/UIGameOver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIGameOver : MonoBehaviour
+{
+    Button restartBttn, mainMenuBttn;
+   
+    void Start()
+    {
+        restartBttn = transform.GetChild(1).GetComponent<Button>();
+        restartBttn.onClick.AddListener(OnRestart);
+        mainMenuBttn = transform.GetChild(2).GetComponent<Button>();
+        mainMenuBttn.onClick.AddListener(OnMainMenu);
+    }
+    void OnRestart()
+    {
+        SceneManager.LoadScene(1);
+    }
+    void OnMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
