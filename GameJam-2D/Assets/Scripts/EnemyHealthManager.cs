761dc840137a86ddd9bf5820d8593197a208c39b
Megaoski <oscar_2596@hotmail.com>
2021-02-04 14:16:06 +0100

    Bullet now hits GO with "Enemy" tag

---
+++ GameJam-2D/Assets/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour
+{
+    public int MaxHealth;
+    public int CurrentHealth;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetMaxHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))//test if enemy dies
+        {
+            CurrentHealth -= 1;
+        }
+
+        if (CurrentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetMaxHealth()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    public void HurtEnemy(int DamageToGive)
+    {
+        CurrentHealth -= DamageToGive;
+    }
+
+}
