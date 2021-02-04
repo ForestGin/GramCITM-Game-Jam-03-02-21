a63eb91c32fe79f2c9e72033bae448703fc06e4f
Pol Bosch <pol.hcsob@gmail.com>
2021-02-04 12:29:50 +0100

    Weapon rotating

---
+++ GameJam-2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Movement_Speed = 5f;
+
+    public Rigidbody2D player_rb;
+    public Rigidbody2D weapon_rb;
+
+    public GameObject weapon;
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mouse_position;
+
+    private void Start()
+    {
+        weapon_rb = weapon.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+    }
+
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mouse_position = cam.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        player_rb.MovePosition(player_rb.position + movement * Movement_Speed * Time.fixedDeltaTime);
+
+        Vector2 lookdir = mouse_position - player_rb.position;
+        float angle = Mathf.Atan2(lookdir.y, lookdir.x) * Mathf.Rad2Deg - 90f;
+        weapon_rb.rotation = angle;
+    }
+}
