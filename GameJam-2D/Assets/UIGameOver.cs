65888f8fd242986ebebea543e2413c1113396117
allenbateman <57528826+allenbateman@users.noreply.github.com>
2021-02-04 13:47:13 +0100

    MenusUpdate

---
+++ GameJam-2D/Assets/UIGameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGameOver : MonoBehaviour
+{
+    Button restartBttn, mainMenuBttn;
+   
+    void Start()
+    {
+        restartBttn = transform.GetChild(1).GetComponent<Button>();
+        restartBttn.onClick.AddListener(OnRestart);
+        mainMenuBttn = transform.GetChild(2).GetComponent<Button>();
+        mainMenuBttn.onClick.AddListener(OnMainMenu);
+    }
+    void OnRestart()
+    {
+        Debug.Log("Game Restarted");
+    }
+    void OnMainMenu()
+    {
+        Debug.Log("OnMainMenu");
+    }
+
+}
