57c5f7678866a1a31e87d45bc8dda7e5ddfc4f6d
Megaoski <oscar_2596@hotmail.com>
2021-02-05 15:46:37 +0100

    Boss updated

--- GameJam-2D/Assets/Scripts/EnemySpriteDirection.cs
+++ GameJam-2D/Assets/Scripts/EnemySpriteDirection.cs
@@ -1,1 +1,21 @@
-﻿//
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemySpriteDirection : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.GetComponent<Rigidbody2D>().velocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (gameObject.GetComponent<Rigidbody2D>().velocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
