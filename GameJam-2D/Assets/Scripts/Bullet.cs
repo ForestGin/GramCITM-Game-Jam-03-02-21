d40ce9f4873ef63caf1daf79f2831d61f16fd2d1
Pol Bosch <pol.hcsob@gmail.com>
2021-02-05 12:50:26 +0100

    Added bullet particle

--- GameJam-2D/Assets/Scripts/Bullet.cs
+++ GameJam-2D/Assets/Scripts/Bullet.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public GameObject explosion;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //Instantiate explosion 
 
+        Instantiate(explosion, transform.position, transform.rotation);
         Destroy(gameObject);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Instantiate(explosion, transform.position, transform.rotation);
         Destroy(gameObject);
     }
 }
